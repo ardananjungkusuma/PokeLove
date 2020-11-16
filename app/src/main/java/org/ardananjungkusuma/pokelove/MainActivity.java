28bb68a1e385b7cbdd6457b3daa308131cc2bff6
ardananjungkusuma <ardananjungkusuma@gmail.com>
2020-11-16 14:15:02 +0700

    adding some features

--- app/src/main/java/org/ardananjungkusuma/pokelove/MainActivity.java
+++ app/src/main/java/org/ardananjungkusuma/pokelove/MainActivity.java
@@ -2,7 +2,9 @@ package org.ardananjungkusuma.pokelove;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void infoClick(View view) {
+        Intent i = new Intent(MainActivity.this, Info.class);
+        startActivity(i);
+    }
+
+    public void elementChart(View view) {
+        Intent i = new Intent(MainActivity.this, ElementChart.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/org/ardananjungkusuma/pokelove/Pokemon.java
@@ -0,0 +1,13 @@
+package org.ardananjungkusuma.pokelove;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pokemon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon);
+    }
+}
