1cfa4a6501ae087dbeaada4d388af4b68f120d5a
ardananjungkusuma <ardananjungkusuma@gmail.com>
2020-11-17 13:58:02 +0700

    adding ball details feature

--- app/src/main/java/org/ardananjungkusuma/pokelove/MainActivity.java
+++ app/src/main/java/org/ardananjungkusuma/pokelove/MainActivity.java
@@ -1,9 +1,12 @@
 package org.ardananjungkusuma.pokelove;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,5 +30,29 @@ public class MainActivity extends AppCompatActivity {
     public void ballDex(View view) {
         Intent i = new Intent(MainActivity.this, BallActivity.class);
         startActivity(i);
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            exitByBackKey();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    protected void exitByBackKey() {
+        AlertDialog alertbox = new AlertDialog.Builder(this)
+                .setMessage("Do you want to exit application?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                    }
+                })
+                .show();
     }
 }
