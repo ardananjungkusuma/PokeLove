d0d15fc839316892d8fe58bb077b18f283a5de08
ardananjungkusuma <ardananjungkusuma@gmail.com>
2020-11-29 21:50:47 +0700

    Adding detail element

--- app/src/main/java/org/ardananjungkusuma/pokelove/ElementDetailActivity.java
+++ app/src/main/java/org/ardananjungkusuma/pokelove/ElementDetailActivity.java
@@ -5,13 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.ardananjungkusuma.pokelove.models.Element;
 
 public class ElementDetailActivity extends AppCompatActivity {
+    private TextView txtName, txtDesc, txtDefense, txtOffense, txtContestProperties;
+    private ImageView imgElement;
+
+    public static final String Key_ElementActivity = "Key_ElementActivity";
+
+    Element element;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_element_detail);
+        txtName = (TextView) findViewById(R.id.detNameElement);
+        txtDesc = (TextView) findViewById(R.id.detDescElement);
+        txtDefense = (TextView) findViewById(R.id.detDefenseElement);
+        txtOffense = (TextView) findViewById(R.id.detOffenseElement);
+        txtContestProperties = (TextView) findViewById(R.id.detContestPropertiesElement);
+        imgElement = (ImageView) findViewById(R.id.detImageElement);
+
+        element = getIntent().getParcelableExtra(Key_ElementActivity);
+
+        txtName.setText(element.getName());
+        txtDesc.setText(element.getDescription());
+        txtDefense.setText(element.getDefense());
+        txtOffense.setText(element.getOffense());
+        txtContestProperties.setText(element.getContestProperties());
+        Picasso.get().load(element.getImg()).into(imgElement);
     }
 
     public void backToElementDex(View view) {
