c3b1f5627b3c25388a84a3e03b8c495994f6e7c2
ardananjungkusuma <ardananjungkusuma@gmail.com>
2020-11-29 21:53:32 +0700

    adding detail title

--- app/src/main/java/org/ardananjungkusuma/pokelove/ElementDetailActivity.java
+++ app/src/main/java/org/ardananjungkusuma/pokelove/ElementDetailActivity.java
@@ -13,7 +13,7 @@ import com.squareup.picasso.Picasso;
 import org.ardananjungkusuma.pokelove.models.Element;
 
 public class ElementDetailActivity extends AppCompatActivity {
-    private TextView txtName, txtDesc, txtDefense, txtOffense, txtContestProperties;
+    private TextView txtTitle,txtName, txtDesc, txtDefense, txtOffense, txtContestProperties;
     private ImageView imgElement;
 
     public static final String Key_ElementActivity = "Key_ElementActivity";
@@ -30,9 +30,11 @@ public class ElementDetailActivity extends AppCompatActivity {
         txtOffense = (TextView) findViewById(R.id.detOffenseElement);
         txtContestProperties = (TextView) findViewById(R.id.detContestPropertiesElement);
         imgElement = (ImageView) findViewById(R.id.detImageElement);
+        txtTitle = (TextView) findViewById(R.id.txtDetailTitle);
 
         element = getIntent().getParcelableExtra(Key_ElementActivity);
 
+        txtTitle.setText(element.getName());
         txtName.setText(element.getName());
         txtDesc.setText(element.getDescription());
         txtDefense.setText(element.getDefense());
