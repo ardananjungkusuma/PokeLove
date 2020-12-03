233f67afe9f6c5cb40898f2a56818daf777ff601
ardananjungkusuma <ardananjungkusuma@gmail.com>
2020-12-03 12:39:03 +0700

    changing font style

--- app/src/main/java/org/ardananjungkusuma/pokelove/adapter/PokemonListAdapter.java
+++ app/src/main/java/org/ardananjungkusuma/pokelove/adapter/PokemonListAdapter.java
@@ -1,11 +1,13 @@
 package org.ardananjungkusuma.pokelove.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
